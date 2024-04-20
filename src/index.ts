5a8e797052004ecee3ae5582bbe2f43eb4139204
Celio Costa <celiorscosta@hotmail.com>
2024-04-20 09:33:23 -0300

    feat: criando fila

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { config } from 'dotenv';
 import express from 'express';
 import Period from './enums/Period';
 import Candle from './Models/Candle';
+import { createMessageChannel } from './messages/messageChannel';
 
 const app = express();
 
@@ -15,24 +16,30 @@ const readMarkedPrice = async (): Promise<number> => {
 }
 
 const generateCandle = async () => {
-    while (true) {
-        const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS;
+    const messageChannel = await createMessageChannel();
+    if (messageChannel) {
+        while (true) {
+            const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS;
+            const candle = new Candle('BTC', new Date());
 
-        const candle = new Candle('BTC', new Date());
+            console.log('------------------------------------------------------');
+            console.log('New candle created');
 
-        console.log('------------------------------------------------------');
-        console.log('New candle created');
+            for (let i = 0; i < loopTimes; i++) {
+                const price = await readMarkedPrice();
+                candle.addValue(price);
+                console.log(`Marked price: ${price} of ${loopTimes}`);
+                await new Promise(resolve => setTimeout(resolve, Period.THIRTY_SECONDS));
+            }
 
-        for (let i = 0; i < loopTimes; i++) {
-            const price = await readMarkedPrice();
-            candle.addValue(price);
-            console.log(`Marked price: ${price} of ${loopTimes}`);
-            await new Promise(resolve => setTimeout(resolve, Period.THIRTY_SECONDS));
+            candle.closeCandle();
+            console.log('Candle closed');
+            const candleObj = candle.toSimpleObject();
+            console.log(candleObj);
+            const candleJson = JSON.stringify(candleObj);
+            messageChannel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(candleJson));
+            console.log('Candle Enqueued');
         }
-
-        candle.closeCandle();
-        console.log('Candle closed');
-        console.log(candle.toSimpleObject());
     }
 }
 
